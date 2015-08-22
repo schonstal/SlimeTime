f25289432d68d43c3089a7895d58295e042b76a1
Josh Schonstal <josh@schonstal.org>
2015-08-22 09:31:38 -0700

    Game jam

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,6 +9,15 @@ class PlayState extends FlxState
 {
   override public function create():Void {
     super.create();
+    FlxG.camera.pixelPerfectRender = false;
+
+    var background = new FlxSprite();
+    background.makeGraphic(FlxG.width, FlxG.height, 0xffffffff);
+    add(background);
+
+    var p = new Player();
+    p.init();
+    add(p);
 
     //DEBUGGER
     FlxG.debugger.drawDebug = true;
