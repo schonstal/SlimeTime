bdbaa75009ae97eabf4cb69fec6f2bcc087099b4
Josh Schonstal <josh@schonstal.org>
2015-08-24 17:31:34 -0700

    Health pickups

---
+++ source/Health.hx
@@ -0,0 +1,40 @@
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxObject;
+import flash.geom.Point;
+import flixel.system.FlxSound;
+import flixel.math.FlxRandom;
+import flixel.math.FlxVector;
+import flixel.group.FlxSpriteGroup;
+
+class Health extends FlxSprite {
+  var initialized:Bool = false;
+
+  public function new() {
+    super();
+  }
+
+  public function spawn():Void {
+    if (!initialized) {
+      loadGraphic("assets/images/healthPickup.png", true, 15, 15);
+      animation.add("flash", [0, 1], 5, true);
+      animation.play("flash");
+    }
+    initialized = true;
+    alive = true;
+    exists = true;
+    velocity.y = 100;
+    y = 0;
+//    acceleration.y = Player.gravity;
+
+//    velocity.y = Reg.random.int(-400, -600);
+
+//    y = FlxG.height;
+    x = Reg.random.int(16, FlxG.width - 32);
+  }
+
+  public override function update(elapsed:Float):Void {
+    if (y > FlxG.height) exists = false;
+    super.update(elapsed);
+  }
+}
---
+++ source/HealthGroup.hx
@@ -0,0 +1,35 @@
+package;
+
+import flixel.group.FlxSpriteGroup;
+import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.util.FlxTimer;
+import flixel.FlxObject;
+
+import flixel.math.FlxVector;
+import flixel.math.FlxMath;
+
+class HealthGroup extends FlxSpriteGroup {
+  var spawnTimer:Float = 5;
+
+  public function new() {
+    super();
+  }
+
+  override public function update(elapsed:Float):Void {
+    if (Reg.started) {
+      spawnTimer -= elapsed;
+      if (spawnTimer < 0) {
+        spawnTimer = Reg.random.float(
+          FlxMath.lerp(7, 5, Reg.difficulty),
+          FlxMath.lerp(10, 7, Reg.difficulty)
+        );
+        var g = recycle(Health);
+        cast(g, Health).spawn();
+        add(g);
+      }
+    }
+
+    super.update(elapsed);
+  }
+}
