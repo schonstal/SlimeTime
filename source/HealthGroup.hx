c94d7d155ffbd41f57b4f83ac3aa869fcbf1b491
Josh Schonstal <josh@schonstal.org>
2015-11-17 22:28:42 -0800

    First pass at health particless

--- source/HealthGroup.hx
+++ source/HealthGroup.hx
@@ -5,15 +5,23 @@ import flixel.FlxSprite;
 import flixel.FlxG;
 import flixel.util.FlxTimer;
 import flixel.FlxObject;
+import flixel.util.FlxColor;
 
 import flixel.math.FlxVector;
 import flixel.math.FlxMath;
 
 class HealthGroup extends FlxSpriteGroup {
-  var spawnTimer:Float = 5;
+  var spawnTimer:Float = 1;
+  var particles:FlxSpriteGroup;
+  var healthSprite:Health;
+
+  var particleTimer:Float = 0;
+  var particleThreshold:Float = 0.025;
 
   public function new() {
     super();
+    particles = new FlxSpriteGroup();
+    add(particles);
   }
 
   override public function update(elapsed:Float):Void {
@@ -24,12 +32,52 @@ class HealthGroup extends FlxSpriteGroup {
           FlxMath.lerp(15, 10, Reg.difficulty),
           FlxMath.lerp(20, 15, Reg.difficulty)
         );
-        var g = recycle(Health);
-        cast(g, Health).spawn();
-        add(g);
+
+        healthSprite = cast(recycle(Health), Health);
+        healthSprite.spawn();
+        add(healthSprite);
       }
     }
 
+    if(healthSprite != null && healthSprite.exists) {
+      spawnParticles(elapsed);
+    }
+
     super.update(elapsed);
+  }
+
+  function spawnParticles(elapsed:Float) {
+    particleTimer += elapsed;
+    if (particleTimer >= particleThreshold) {
+      var particle:FlxSprite = particles.recycle(FlxSprite);
+      var size = Reg.random.int(0, 2);
+
+      particle.loadGraphic("assets/images/healthParticle.png", true, 5, 5);
+      particle.animation.add("0", [0]);
+      particle.animation.add("1", [0]);
+      particle.animation.add("2", [1]);
+      particle.animation.play('$size');
+
+      particle.color = FlxColor.fromHSB(336,
+        size > 1 ? 0.9 : 0.1,
+        Reg.random.float(0.8, 1)
+      );
+
+      particle.x = healthSprite.x + Reg.random.int(0, 12);
+      particle.y = healthSprite.y;
+
+      particle.velocity.x = Reg.random.int(-10, 10);
+      particle.velocity.y = Reg.random.int(-10, 0);
+
+      particle.exists = true;
+      particle.visible = true;
+      particle.solid = false;
+
+      new FlxTimer().start(Reg.random.float(0.3, 0.6), function(t) {
+        particle.exists = false;
+      });
+
+      particleTimer = 0;
+    }
   }
 }
