bf51f8829fb9427e99461c39661cd5de0da27170
Josh Schonstal <josh@schonstal.org>
2016-01-10 13:45:37 -0800

    Add particles to enemy lasers

--- source/EnemyLaserGroup.hx
+++ source/EnemyLaserGroup.hx
@@ -11,10 +11,16 @@ import flixel.math.FlxVector;
 class EnemyLaserGroup extends FlxSpriteGroup {
   public var onCompleteCallback:Void->Void;
 
+  var lasers:FlxSpriteGroup;
+  var particles:FlxSpriteGroup;
+
   var duration:Float = 0;
 
   public function new(Y:Float):Void {
     super();
+
+    lasers = new FlxSpriteGroup();
+    particles = new FlxSpriteGroup();
 
     for (i in (0...Std.int((FlxG.width - 32)/32))) {
       var laserSprite = new FlxSprite(16 + i * 32, 0);
@@ -28,17 +34,37 @@ class EnemyLaserGroup extends FlxSpriteGroup {
       laserSprite.setFacingFlip(FlxObject.LEFT, false, false);
       laserSprite.animation.callback = onAnimate;
       laserSprite.animation.finishCallback = onAnimationComplete;
-      add(laserSprite);
+      lasers.add(laserSprite);
     }
+
+    var particleSprite = new FlxSprite();
+    particleSprite.loadGraphic("assets/images/projectiles/enemy/laserSplash.png", true, 16, 48);
+    particleSprite.animation.add("splash", [0, 1, 2, 3], 15, true);
+    particleSprite.setFacingFlip(FlxObject.RIGHT, true, false);
+    particleSprite.setFacingFlip(FlxObject.LEFT, false, false);
+    particleSprite.y = -22;
+    particles.add(particleSprite);
+
+    add(lasers);
+    add(particles);
 
     y = Y + 5;
   }
 
   public function initialize(Y:Float):Void {
     exists = true;
-    for(laserSprite in members) {
+    lasers.exists = true;
+    particles.exists = true;
+
+    for(particleSprite in particles.members) {
+      particleSprite.exists = true;
+      particleSprite.solid = false;
+    }
+
+    for(laserSprite in lasers.members) {
       laserSprite.exists = true;
     }
+
     y = Y + 5;
   }
 
@@ -50,17 +76,28 @@ class EnemyLaserGroup extends FlxSpriteGroup {
 
   public function shoot(facing:Int, duration:Float):Void {
     this.duration = duration;
-    for(laserSprite in members) {
+
+    for(laserSprite in lasers.members) {
       laserSprite.animation.play("shoot");
       laserSprite.solid = true;
       laserSprite.facing = facing;
+    }
+
+    for(particleSprite in particles.members) {
+      particleSprite.animation.play("splash");
+      particleSprite.facing = facing;
+      particleSprite.x = facing == FlxObject.RIGHT ? 16 : FlxG.width - 16 - particleSprite.width;
     }
   }
 
   function onAnimate(name:String, frame:Int, frameIndex:Int):Void {
     if (name == "shoot" && frame == 3) {
       if (duration <= 0) {
-        for(laserSprite in members) {
+        for(particleSprite in particles.members) {
+          particleSprite.exists = false;
+        }
+
+        for(laserSprite in lasers.members) {
           laserSprite.solid = false;
           laserSprite.animation.play("fade");
         }
