23b2ee8665c897bf64e76bc4efe2c539686c4624
Josh Schonstal <josh@schonstal.org>
2016-06-21 23:07:50 -0700

    Name hard mode 'extreme'

--- source/MainMenuGroup.hx
+++ source/MainMenuGroup.hx
@@ -21,7 +21,7 @@ class MainMenuGroup extends FlxSpriteGroup {
     buttons = new Array<MainMenuButton>();
     buttons[0] = new MainMenuButton("options", function() { showOptions(0); });
     buttons[1] = new MainMenuButton("start", function() { startGame(); });
-    buttons[2] = new MainMenuButton("?????", function() { startHardMode(); });
+    buttons[2] = new MainMenuButton("extreme", function() { startHardMode(); });
 
     for (i in (0...3)) {
       buttons[i].x = (i + 1) * 80;
